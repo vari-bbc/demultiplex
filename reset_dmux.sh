3a287309494fcc12616bfae037d04b0205d8c54f
genomics-kl <kin.lau@vai.org>
2020-12-03 16:02:10 -0500

    For reset_dmux.sh, add snakemake log directory to files to delete and containue deleting even if not all target files present.

--- reset_dmux.sh
+++ reset_dmux.sh
@@ -9,17 +9,17 @@ export basecalls_dir
 
 projects=$(perl -F/,/ -lane 'print qq/$ENV{'basecalls_dir'}$F[9]/ if ($F[9] =~ /^\S/ && $F[9] ne q/Sample_Project/)' SampleSheet.csv  | sort | uniq)
 
-target_files="$projects bcl2fastq.done bcl2fastq.log diagnostic_files ${basecalls_dir}Undetermined* ${basecalls_dir}Stats* ${basecalls_dir}Reports*"
-
-#ls $target_files
+target_files="$projects bcl2fastq.done bcl2fastq.log diagnostic_files ${basecalls_dir}Undetermined* ${basecalls_dir}Stats* ${basecalls_dir}Reports* snakemake_job_logs"
 
 if ls $target_files >/dev/null 2>&1; then
-  ls --color $target_files
+  echo "Target files found."
 else
-  echo "ERROR: Some dmux output files do not exist. In case you are trying to delete something unintentionally, this script will exit now."
-  ls $target_files
-  exit 1 # should error out from above line but putting this here for extra security
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo "WARNING: Some dmux output files do not exist. Double-check that you are in a dmux directory."
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 fi
+
+ls --color $target_files
 
 while true; do
     read -p "Do you wish to delete the above files?" yn
