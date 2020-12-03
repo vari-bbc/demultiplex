8aeb80045feed441681a590e84d9e8ff6c65a952
genomics-kl <kin.lau@vai.org>
2020-12-03 17:26:54 -0500

    Some changes to fastq_md5sums.sh to handle when there are no fastq.gz files

--- fastq_md5sums.sh
+++ fastq_md5sums.sh
@@ -6,4 +6,11 @@ set -o pipefail
 
 module load bbc/parallel/parallel-20191122
 
-parallel -k --will-cite -j 16 md5sum ::: $(find Data/Intensities/BaseCalls/ -name '*fastq.gz' | sort) > md5sums.txt
+target_files=$(find Data/Intensities/BaseCalls/ -name '*fastq.gz' | sort)
+
+if [ -z "$target_files" ]; then
+    echo "No fastq.gz files found."
+    exit 1
+fi
+
+parallel -k --will-cite -j 8 md5sum ::: $target_files > md5sums.txt
