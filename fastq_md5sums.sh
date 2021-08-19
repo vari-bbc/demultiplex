0d8878a89d81a0794c4383c5a4f3a7b9d6851f98
heyyyjude <jay.kim@vai.org>
2021-08-19 14:30:33 -0400

    add md5sum rule

--- fastq_md5sums.sh
+++ fastq_md5sums.sh
@@ -7,6 +7,8 @@ set -o pipefail
 module load bbc/parallel/parallel-20191122
 
 target_files=$(find Data/Intensities/BaseCalls/ -name '*fastq.gz' | sort)
+## skip undetermined*.fastq.gz
+#target_files=$(find Data/Intensities/BaseCalls/ -name '*fastq.gz' ! -name "Undetermined*.fastq.gz" | sort)
 
 if [ -z "$target_files" ]; then
     echo "No fastq.gz files found."
