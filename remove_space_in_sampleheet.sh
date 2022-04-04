a606dc0d8c128b8646becf2a7419af3bc12dec9b
J Kim <heyyyjude@users.noreply.github.com>
2022-04-04 14:03:49 -0400

    create remove_space_in_sampleheet.sh

--- remove_space_in_sampleheet.sh
+++ remove_space_in_sampleheet.sh
@@ -7,14 +7,14 @@ set -o pipefail
 if [ $# -ne 1 ]
 then
     echo "How to use"
-    echo "bash remove_whitespace.sh [SampleSheet.csv]"
+    echo "bash remove_space_in_sampleheet.sh [SampleSheet.csv]"
     exit 1
 fi
 
 if [ -e SampleSheet.csv ]
 then
     sed -i -e "s/ //g" $1
-    echo "whitespace is removed in $1"
+    echo "Space characters are removed in $1"
 else
     echo "$1 is not found!"
 fi
